89e5bd3dcc712690dc9523bf1d3fad16ed4fcbb0
Jason Lafferty <jason_d_lafferty@hotmail.com>
2017-08-15 08:56:08 +0100

    Add support for redux devtools

--- src/app/client/redux/store/configureStore.js
+++ src/app/client/redux/store/configureStore.js
@@ -1,12 +1,26 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    actionsBlacklist: []
+  })
+  : compose;
+/* eslint-enable */
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
+    composeEnhancers(
+      applyMiddleware(
+        thunk,
+        reduxImmutableStateInvariant()
+      )
+    )
   );
 }
