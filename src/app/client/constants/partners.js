e684ba18275dc3ce9cdd4e50da6e192f9a18bfe3
Jason Lafferty <jason_d_lafferty@hotmail.com>
2017-08-15 01:36:58 +0100

    App partners as constants

---
+++ src/app/client/constants/partners.js
@@ -0,0 +1,17 @@
+export default [{
+    name: 'Airbnb',
+    logo: require('../../static/images/Airbnb.png')
+  },
+  {
+    name: 'Metro',
+    logo: require('../../static/images/Metro.png')
+  },
+  {
+    name: 'Pariti',
+    logo: require('../../static/images/Pariti.png')
+  },
+  {
+    name: 'Unshackled',
+    logo: require('../../static/images/Unshackled.png')
+  }
+];
